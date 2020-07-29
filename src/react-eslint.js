7956c1c87ca9c7dcaa375fde05d24bd778f17db2
vipulnema2610 <vipul@infoedge.com>
2020-07-29 18:18:14 +0530

    github repo setup .......................

---
+++ src/react-eslint.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class ReactEslint extends Component {
+  render() {
+    return <div>ESlint component</div>;
+  }
+}
--- src/test-eslint.js
+++ src/test-eslint.js
@@ -1,1 +1,16 @@
-var a = "a";
+const a = 'a';
+console.log(a);
+if (1) {
+  const b = 6;
+  console.warn(b);
+}
+
+const obj = {
+  prop: 2,
+};
+
+console.log(obj);
+// var a = 2;
+if (obj.hasOwnProperty('prop')) {
+  const c = 2 + 2;
+}
